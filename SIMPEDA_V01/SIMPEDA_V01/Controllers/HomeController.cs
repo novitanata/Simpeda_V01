f8b1d9245c6acbfa9fbb653016b0b691d07f5762
Ika Ayu Rahmania <ayufow@gmail.com>
2014-06-14 13:21:13 +0700

    Login & Registrasi View

--- SIMPEDA_V01/SIMPEDA_V01/Controllers/HomeController.cs
+++ SIMPEDA_V01/SIMPEDA_V01/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Data.Linq.Mapping;
 using System.Collections;
 using System.Reflection;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Mvc;
@@ -39,5 +40,59 @@ namespace SIMPEDA_V01.Controllers
         //{ 
             
         //}
+        //protected void Login1_LoggingIn(object sender, LoginCancelEventArgs e)
+        //{
+        //    //Check to see if the current user exists
+        //    if (Membership.GetUser() != null)
+        //    {
+        //        //Check to see if the user is currently locked out
+        //        if (Membership.GetUser(Login1.UserName).IsLockedOut)
+        //        {
+        //            //Get the last lockout  date from the user
+        //            DateTime lastLockout = Membership.GetUser(Login1.UserName).LastLockoutDate;
+        //            //Calculate the time the user should be unlocked
+        //            DateTime unlockDate = lastLockout.AddMinutes(Membership.PasswordAttemptWindow);
+
+        //            //Check to see if it is time to unlock the user
+        //            //if (DateTime.Now > unlockDate)
+        //            //    Membership.GetUser(Login1.UserName).UnlockUser();
+        //        }
+        //    }
+        //}
+       // public virtual bool IsLockedOut { get; }
+        public bool ValidateUser(string idUser, string passwordUser)
+        {
+            bool ret = false;
+            try
+            {
+                using (db)
+                {
+                    if (passwordUser != null)
+                    {
+                        //Mahasiswa mhs = (from mahasiswa in db.Mahasiswas
+                        //                 wfhere mahasiswa.NRP == idUser
+                        //                 select mahasiswa).SingleOrDefault();
+                        Dosen dos = (from dosen in db.Dosens
+                                     where dosen.NIP == idUser
+                                     select dosen).SingleOrDefault();
+                        if (dos != null)
+                        {
+                            //if (dos.IsLockedOut)
+                            //{ 
+                            
+                            //}
+                        }
+                        
+                    }
+                    
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return ret;
+        }
 	}
 }
