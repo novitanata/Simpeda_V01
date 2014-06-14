dee501b35b3ba5d0767616ff19c22d7ad8915997
Ika Ayu Rahmania <ayufow@gmail.com>
2014-06-14 07:29:32 +0700

    Login View

--- SIMPEDA_V01/SIMPEDA_V01/Controllers/HomeController.cs
+++ SIMPEDA_V01/SIMPEDA_V01/Controllers/HomeController.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.SqlClient;
+using System.Data.Entity;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Collections;
+using System.Reflection;
 using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using System.Web.Mvc;
 using SIMPEDA_V01.Models;
 
-namespace SIMPEDA_V01.Controllers
+
+namespace SIMPEDA_V01.Controllers   
 {
     public class HomeController : Controller
     {
+        private SimpedaEntities db = new SimpedaEntities();
+        
         //
         // GET: /Home/
         public ActionResult Index()
@@ -18,5 +29,15 @@ namespace SIMPEDA_V01.Controllers
             ViewBag.Pengembalian = "Pengembalian";
             return View();
         }
+
+        //protected void Page_Load(object sender, EventArgs e)
+        //{ 
+        
+        //}
+
+        //protected void btnMasuk_Click(object sender, EventArgs e)
+        //{ 
+            
+        //}
 	}
 }
