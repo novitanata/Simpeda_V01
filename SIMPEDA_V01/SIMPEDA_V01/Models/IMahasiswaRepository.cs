99e24895c7e85e53eb7d95483bc8a6ab51a7bbf1
Aisha Yuliandari <aishayuliandari@gmail.com>
2014-06-16 15:45:44 +0700

    Interface Mahasiswa Repository

---
+++ SIMPEDA_V01/SIMPEDA_V01/Models/IMahasiswaRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace SIMPEDA_V01.Models
+{
+    public class IMahasiswaRepository
+    {
+        List<Mahasiswa> GetAllMahasiswa();
+
+        Mahasiswa GetMahasiswaById(int id);
+        void AddMahasiswa(Mahasiswa mahasiswa);
+        void UpdateMahasiswa(Mahasiswa mahasiswa);
+        void DeleteMahasiswa(Mahasiswa mahasiswa);
+        void Save();
+    }
+}
